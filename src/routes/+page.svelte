88d5729327f84e6442d976fd360c61fc7c7fde2f
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2023-01-19 09:44:04 -0500

    :sparkles: Add super simple jar that fills with marbles.
    This was done with significant help from ChatGPT

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script>
     export let data;
+
+    import '$lib/assets/global.css';
+    import { enhance } from '$app/forms';
+    import MarbleJar from './MarbleJar.svelte';
 </script>
 
 <h1>Haz tu ofrecimiento por convivio</h1>
 <p>{data.currentPrayers}</p>
 
-<form method="POST">
+<form method="POST" use:enhance>
     <button>Haz tu ofrecimiento</button>
 </form>
+
+<MarbleJar marbles={data.currentPrayers} />
---
+++ src/routes/MarbleJar.svelte
@@ -0,0 +1,49 @@
+<script>
+    export let marbles = 0;
+
+    let colors = ['#f44336', '#9c27b0', '#3f51b5', '#03a9f4', '#4caf50', '#ffc107'];
+    function getRandomColor() {
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
+    function getMarblePosition(i) {
+        let x = i % 10;
+        let y = Math.floor(i / 10);
+        let offset = y % 2 === 0 ? 0 : 5;
+        let left = x * 10 + offset;
+        let top = 90 - y * 8;
+        return { left, top };
+    }
+</script>
+
+<div class="jar-4">
+    <div class="marble-container">
+        {#each Array.from({ length: marbles }) as _, i}
+            <div
+                class="marble"
+                style="background-color: {getRandomColor()}; top: {getMarblePosition(i)
+                    .top}%; left: {getMarblePosition(i).left}%;"
+            />
+        {/each}
+    </div>
+</div>
+
+<style>
+    .jar-4 {
+        width: min-content;
+        background-color: #fff;
+        overflow: hidden;
+    }
+
+    .marble-container {
+        width: 200px;
+        height: 250px;
+        position: relative;
+    }
+
+    .marble {
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        position: absolute;
+    }
+</style>
