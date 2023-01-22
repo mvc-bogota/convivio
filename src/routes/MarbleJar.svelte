53276af990b8f14456ad9a6f64496ef7cc39d508
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2023-01-22 18:04:19 -0500

    :art: Simplify logic of building the grid of marbles

--- src/routes/MarbleJar.svelte
+++ src/routes/MarbleJar.svelte
@@ -2,48 +2,40 @@
     export let marbles = 0;
 
     let colors = ['#1855B3', '#506481', '#67DAE5', '#03a9f4', '#EC8C6C', '#B42C2C'];
+
     function getRandomColor() {
         return colors[Math.floor(Math.random() * colors.length)];
     }
-    function getMarblePosition(i) {
-        let x = i % 10;
-        let y = Math.floor(i / 10);
-        let offset = y % 2 === 0 ? 0 : 5;
-        let left = x * 10 + offset;
-        let top = 90 - y * 8;
-        return { left, top };
-    }
 </script>
 
-<div class="jar-4">
+<div class="jar">
     <div class="marble-container">
         {#each Array.from({ length: marbles }) as _, i}
-            <div
-                class="marble"
-                style="background-color: {getRandomColor()}; top: {getMarblePosition(i)
-                    .top}%; left: {getMarblePosition(i).left}%;"
-            />
+            <div class="marble" style="background-color: {getRandomColor()};" />
         {/each}
     </div>
 </div>
 
 <style>
-    .jar-4 {
-        width: min-content;
+    .jar {
+        --jar-width: 20em;
+        --jar-height: 30em;
+        width: var(--jar-width);
+        height: var(--jar-height);
         background-color: #fff;
-        overflow: hidden;
+        display: flex;
+        flex-direction: column-reverse;
     }
 
     .marble-container {
-        width: 200px;
-        height: 250px;
-        position: relative;
+        display: flex;
+        flex-wrap: wrap-reverse;
+        align-items: end;
     }
 
     .marble {
-        width: 10px;
-        height: 10px;
+        flex-basis: calc(var(--jar-width) / 10);
+        height: calc(var(--jar-height) / 15);
         border-radius: 50%;
-        position: absolute;
     }
 </style>
