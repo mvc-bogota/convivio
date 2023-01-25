9ff9b6dbdae7276a4f541fe08c3aa1c4c5223caf
Juan Pablo Castiblanco <juanpablocj@gmail.com>
2023-01-25 13:35:48 -0500

    :hammer: Added new containers to prayers page

--- src/routes/prayers/+page.svelte
+++ src/routes/prayers/+page.svelte
@@ -6,16 +6,26 @@
     import { prayerTypes } from '$lib/prayers';
 </script>
 
-<div class="offerings-container">
-    <h1>Haz tu ofrecimiento por Convivio</h1>
-    <p>Hasta el momento llevamos:</p>
-    <ul>
-        {#each Object.entries(prayerTypes) as [type, { color, label }]}
-            <li style="color: {color};">{label}: <strong>{data.prayerTracker.getPrayersCountByType(type)}</strong></li>
-        {/each}
-    </ul>
+<div id="offerings-container">
+    <div id="header-container">
+        <h1>¡Haz tu ofrecimiento por Convivio!</h1>
+        <p>Por cada ofrecimiento que registres, se agrega una bolita al jarrón</p>
+        <p>Cuando un jarrón se llena, se agrega un nuevo jarrón</p>
+    </div>
+    <div id="banner-image"></div>
+
+    <div id="prayer-count-container">
+        <p><strong>Hasta el momento llevamos:</strong></p>
+        <ul>
+            {#each Object.entries(prayerTypes) as [type, { color, label }]}
+                <li style="color: {color};">{label}: <strong>{data.prayerTracker.getPrayersCountByType(type)}</strong></li>
+            {/each}
+        </ul>
+    </div>
 
     <PrayerForm />
+
+    <h2>Jarrones:</h2>
 
     <MarbleJar prayerTracker={data.prayerTracker} />
 </div>
