950c3ba313fd98a86c3e62cd7c2e70e429a043da
Juan Pablo <juanpablocj@gmail.com>
2023-01-31 15:00:51 -0500

    :hammer: Added reference data for concert tickets

---
+++ src/lib/concert-tickets/index.js
@@ -0,0 +1,34 @@
+const singleTicketPrice = 20000;
+
+const calcStandardPrice = function () {
+    return singleTicketPrice * this.quantity;
+};
+
+const calcSpecialPrice = function () {
+    return singleTicketPrice * (this.quantity-1);
+};
+
+const ticketsOptions = {
+    one_ticket: {
+        quantity: 1,
+        price: calcStandardPrice
+    },
+    two_tickets: {
+        quantity: 2,
+        price: calcStandardPrice
+    },
+    three_tickets: {
+        quantity: 3,
+        price: calcStandardPrice
+    },
+    four_tickets: {
+        quantity: 4,
+        price: calcStandardPrice
+    },
+    five_tickets: {
+        quantity: 5,
+        price: calcSpecialPrice
+    },
+};
+
+export { ticketsOptions };
