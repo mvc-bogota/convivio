3624a9966152c01b428a2e061410e07d8a8b7137
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2023-02-02 09:34:17 -0500

    :rotating_light: Run formatter in files

--- src/lib/concert-tickets/index.js
+++ src/lib/concert-tickets/index.js
@@ -5,29 +5,29 @@ const calcStandardPrice = function () {
 };
 
 const calcSpecialPrice = function () {
-    return singleTicketPrice * (this.quantity-1);
+    return singleTicketPrice * (this.quantity - 1);
 };
 
 const ticketsOptions = {
     one_ticket: {
         quantity: 1,
-        price: calcStandardPrice
+        price: calcStandardPrice,
     },
     two_tickets: {
         quantity: 2,
-        price: calcStandardPrice
+        price: calcStandardPrice,
     },
     three_tickets: {
         quantity: 3,
-        price: calcStandardPrice
+        price: calcStandardPrice,
     },
     four_tickets: {
         quantity: 4,
-        price: calcStandardPrice
+        price: calcStandardPrice,
     },
     five_tickets: {
         quantity: 5,
-        price: calcSpecialPrice
+        price: calcSpecialPrice,
     },
 };
 
