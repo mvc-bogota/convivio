9de0795b01bde8ed48913d3185ee8b41ce3ccea5
Juan Pablo <juanpablocj@gmail.com>
2023-02-06 11:22:38 -0500

    :sparkles: Add function that sends confirmation email through SendGrid API

---
+++ src/lib/server/email-generation.js
@@ -0,0 +1,36 @@
+import { SENDGRID_API_KEY } from '$env/static/private';
+import { generateQR } from './qr-generation';
+
+const client = require('@sendgrid/mail');
+
+const verifiedSenderEmail = "noticiasmvcbog@gmail.com"
+const confirmationEmailTemplateId = "d-b81cb950599f4e7aa9b003cbfe098b5b"
+
+export const sendConfirmationEmail = function (databaseEntityId, recipientsEmail, fullName) {
+    client.setApiKey(SENDGRID_API_KEY)
+    
+    const message = {
+        "from": {
+            "email": verifiedSenderEmail
+        },
+        "personalizations": [
+            {
+                "to": [
+                    {
+                        "email": recipientsEmail
+                    }
+                ],
+                "dynamic_template_data": {
+                    "full_name": fullName,
+                    "qr_code_url": generateQR(databaseEntityId)
+                }
+            }
+        ],
+        "template_id": confirmationEmailTemplateId
+    }
+
+    client
+    .send(message)
+    .then(() => console.log(`Mail sent succesfully to ${recipientsEmail}`))
+    .catch(err => console.error(err))
+}
