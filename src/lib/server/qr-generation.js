8614be24653fe3b74371cf657b9910260e0ea3b2
Juan Pablo <juanpablocj@gmail.com>
2023-02-06 11:21:52 -0500

    :sparkles: Add function to generate a QR that redirects to a verification URL

---
+++ src/lib/server/qr-generation.js
@@ -0,0 +1,5 @@
+import { PUBLIC_BASE_URL } from '$env/static/public';
+
+export const generateQR = function (databaseEntityId) {
+    let redirectURL = `${PUBLIC_BASE_URL}/concert-tickets/validation?id=${databaseEntityId}`
+}
