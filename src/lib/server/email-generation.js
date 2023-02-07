eb24d61a49bf15ef82d8f597a9f4ee7d744d6555
Juan Pablo <juanpablocj@gmail.com>
2023-02-07 09:30:11 -0500

    :wrench: Updated email generation function

--- src/lib/server/email-generation.js
+++ src/lib/server/email-generation.js
@@ -1,11 +1,11 @@
 import { SENDGRID_API_KEY } from '$env/static/private';
-import { generateQR } from './qr-generation';
+import { generateQrUrl } from './qr-generation';
 import client from '@sendgrid/mail';
 
 const verifiedSenderEmail = 'noticiasmvcbog@gmail.com';
 const confirmationEmailTemplateId = 'd-b81cb950599f4e7aa9b003cbfe098b5b';
 
-export const sendConfirmationEmail = function (databaseEntityId, recipientsEmail, fullName) {
+export const sendConfirmationEmail = async function (databaseEntityId, recipientsEmail, fullName) {
     client.setApiKey(SENDGRID_API_KEY);
 
     const message = {
@@ -21,15 +21,18 @@ export const sendConfirmationEmail = function (databaseEntityId, recipientsEmail
                 ],
                 dynamic_template_data: {
                     full_name: fullName,
-                    qr_code_url: generateQR(databaseEntityId),
+                    qr_code_url: generateQrUrl(databaseEntityId),
                 },
             },
         ],
         template_id: confirmationEmailTemplateId,
     };
 
-    return client
-        .send(message)
-        .then(() => console.info(`Mail sent succesfully to ${recipientsEmail}`))
-        .catch((err) => console.error(err));
+    try {
+        await client
+            .send(message);
+        return console.info(`Mail sent succesfully to ${recipientsEmail}`);
+    } catch (err) {
+        return console.error(err);
+    }
 };
