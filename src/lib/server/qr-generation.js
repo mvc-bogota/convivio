763191af233eb03815ffb849290c7cf1d902bf4e
Juan Pablo <juanpablocj@gmail.com>
2023-02-07 09:29:28 -0500

    :wrench: Fixed function that generates the QR API URL and renamed it

--- src/lib/server/qr-generation.js
+++ src/lib/server/qr-generation.js
@@ -1,6 +1,9 @@
 import { PUBLIC_BASE_URL } from '$env/static/public';
 
-export const generateQR = function (databaseEntityId) {
-    let redirectURL = `${PUBLIC_BASE_URL}/concert-tickets/validation?id=${databaseEntityId}`;
-    return redirectURL;
+export const generateQrUrl = function (databaseEntityId) {
+    const qrApiUrl = 'https://api.qrserver.com/v1/create-qr-code/?data=';
+    const redirectUrl= `${PUBLIC_BASE_URL}/concert-tickets/validation?id=${databaseEntityId}`;
+
+    const completeUrl = qrApiUrl + encodeURI(redirectUrl);
+    return completeUrl;
 };
