e8a3833e7c720d67afd52fe64c64b35d69565312
Juan Pablo <juanpablocj@gmail.com>
2023-02-10 10:00:14 -0500

    :speech_balloon: Enhanced text literals

--- src/routes/concert-tickets/payment-finished/+page.svelte
+++ src/routes/concert-tickets/payment-finished/+page.svelte
@@ -8,24 +8,26 @@
 
     {#if status === 'APPROVED'}
         <p>
-            ¡Tu pago fue aprobado! A tu correo debería llegar pronto el código QR que tienes que mostrar
+            ¡Tu pago fue <strong>aprobado</strong>! A tu correo debería llegar pronto el código QR que tienes que mostrar
             el día del concierto. Asegúrate de revisar tu bandeja de Spam/Social si no encuentras el correo.
         </p>
     {:else if status === 'DECLINED'}
         <p>
-            Lastimosamente tu pago fue rechazado. Puedes volver a intentar
+            Lastimosamente tu pago fue <strong>rechazado</strong>. Puedes volver a intentar
             <a href="/concert-tickets">aquí</a>
         </p>
     {:else}
         <p>
-            Tu transacción está pendiente. Muy pronto sabremos el estado de tu pago y nos pondremos en
-            contacto contigo con más información sobre tus entradas para el concierto Convivio
+            Tu transacción está <strong>pendiente</strong>. Tan pronto como confirmemos si tu transacción fue aprobada, a tu correo debería llegar 
+            el código QR que tienes que mostrar el día del concierto. Asegúrate de revisar tu bandeja de Spam/Social si no encuentras el correo.
+
+            Si tu transacción es rechazada, vuelve a intentar la compra de tu boleta.
         </p>
     {/if}
 
     <p>
-        El número de referencia de tu transacción es: <strong>{reference}</strong>. Si tienes alguna
-        duda, puedes contactarte con nosotros al siguiente correo: noticiasmvcbog@gmail.com
+        El número de referencia de esta transacción es: <strong>{reference}</strong>. Si tienes alguna
+        duda, puedes contactarte con nosotros al siguiente correo: <a href="mailto:noticiasmvcbog@gmail.com">noticiasmvcbog@gmail.com</a>
     </p>
     <p>
         <a href="/concert-tickets">Volver al inicio</a>
