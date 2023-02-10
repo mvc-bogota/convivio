f013a025ad157712abbabe995f0643f60ffa14d4
Juan Pablo <juanpablocj@gmail.com>
2023-02-10 09:25:31 -0500

    :wrench: Updated "payment finished" HTML

--- src/routes/concert-tickets/payment-finished/+page.svelte
+++ src/routes/concert-tickets/payment-finished/+page.svelte
@@ -3,29 +3,32 @@
     const { reference, status } = data.payment;
 </script>
 
-<h1>Hiciste un pago</h1>
+<div class="wrapper">
+    <h1>¡Hiciste un pago!</h1>
 
-{#if status === 'APPROVED'}
-    <p>
-        ¡Tu pago fue aprobado! Muy pronto nos pondremos en contacto contigo con la información de
-        tus entradas para el concierto Convivio.
-    </p>
-{:else if status === 'DECLINED'}
-    <p>
-        Lastimosamente tu pago fue rechazado. Puedes volver a intentar
-        <a href="/concert-tickets">aquí</a>
-    </p>
-{:else}
-    <p>
-        Tu transacción está pendiente. Muy pronto sabremos el estado de tu pago y nos pondremos en
-        contacto contigo con más información sobre tus entradas para el concierto Convivio
-    </p>
-{/if}
+    {#if status === 'APPROVED'}
+        <p>
+            ¡Tu pago fue aprobado! A tu correo debería llegar pronto el código QR que tienes que mostrar
+            el día del concierto. Asegúrate de revisar tu bandeja de Spam/Social si no encuentras el correo.
+        </p>
+    {:else if status === 'DECLINED'}
+        <p>
+            Lastimosamente tu pago fue rechazado. Puedes volver a intentar
+            <a href="/concert-tickets">aquí</a>
+        </p>
+    {:else}
+        <p>
+            Tu transacción está pendiente. Muy pronto sabremos el estado de tu pago y nos pondremos en
+            contacto contigo con más información sobre tus entradas para el concierto Convivio
+        </p>
+    {/if}
 
-<p>
-    El número de referencia de tu transacción es: <strong>{reference}</strong>. Si tienes alguna
-    duda, puedes contactarte con nosotros.
-</p>
-<p>
-    <a href="/concert-tickets">Volver al inicio</a>
-</p>
+    <p>
+        El número de referencia de tu transacción es: <strong>{reference}</strong>. Si tienes alguna
+        duda, puedes contactarte con nosotros al siguiente correo: noticiasmvcbog@gmail.com
+    </p>
+    <p>
+        <a href="/concert-tickets">Volver al inicio</a>
+    </p>
+</div>
+
