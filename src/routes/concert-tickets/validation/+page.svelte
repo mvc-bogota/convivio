808b4c1a69aa3f6c4c2279c7dc4cc0011e88230a
Juan Pablo <juanpablocj@gmail.com>
2023-03-08 22:23:38 -0500

    :wrench: Updated validation page UI

--- src/routes/concert-tickets/validation/+page.svelte
+++ src/routes/concert-tickets/validation/+page.svelte
@@ -3,4 +3,41 @@
 </script>
 
 <h1>Datos de la compra:</h1>
-<p>El ID de la base de datos es: {data.entityId}</p>
+<table>
+    <tr>
+        <th>Transacción #</th>
+        <td>{data.purchaseId}</td>
+    </tr>
+    <tr>
+        <th>Cantidad de boletas</th>
+        <td>{data.ticketsQuantity}</td>
+    </tr>
+    <tr>
+        <th>Nombre del comprador</th>
+        <td>{data.customerName}</td>
+    </tr>
+    <tr>
+        <th>Correo registrado del comprador</th>
+        <td>{data.customerEmail}</td>
+    </tr>
+</table>
+
+<style>
+    h1 {
+        background-color: var(--color-quaternary);
+        color: var(--color-neutral-light);
+        text-align: center;
+    }
+
+    table {
+        border: 3px solid var(--color-secondary);
+    }
+
+    tr {
+        border: 1px solid var(--color-secondary);
+    }
+
+    th {
+        font-family: var(--ff-secondary);
+    }
+</style>
