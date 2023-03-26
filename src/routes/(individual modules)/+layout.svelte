ab88df9ec1143ebaa1452f2c99d78b75c2b52631
Juan Pablo <juanpablocj@gmail.com>
2023-03-26 09:54:31 -0500

    :truck: moved concert tickets and prayers modules to a common group

---
+++ src/routes/(individual modules)/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    /** @type {import('./$types').LayoutData} */
+    export let data;
+</script>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css" />
+
+<slot/>
