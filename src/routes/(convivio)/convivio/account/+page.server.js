0a72f62fa2c425ca8f5b5956f429d931b65024cc
Juan Bernardo Benavides <jbbenavidesr@unal.edu.co>
2023-04-01 23:50:54 -0500

    :bug: Update correct error function from svelte

--- src/routes/(convivio)/convivio/account/+page.server.js
+++ src/routes/(convivio)/convivio/account/+page.server.js
@@ -1,4 +1,4 @@
-import { fail, redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
 export const load = (async ({ locals: { supabase, getSession } }) => {
 	const session = await getSession();
@@ -28,7 +28,7 @@ export const actions = {
 
 		const session = await getSession();
 
-		const { error } = await supabase.from('profiles').upsert({
+		const { error: profileError } = await supabase.from('profiles').upsert({
 			id: session?.user.id,
 			first_name: firstName,
 			last_name: lastName,
@@ -39,8 +39,8 @@ export const actions = {
 			updated_at: new Date()
 		});
 
-		if (error) {
-			return fail(500, {
+		if (profileError) {
+			return error(500, {
 				firstName,
 				lastName,
 				email,
